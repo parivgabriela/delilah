09ecb72b1d8a678c133ee422b5099439d5a2df4d
parivgabriela <pari.gabriela@gmail.com>
2020-08-12 20:37:07 -0300

    actualizacion de ultils para poder establecer de antemano mensajes y asi poder modificarlos sin problemas

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser=require('body-parser');
 const jwt = require("jsonwebtoken");
 const Sequelize = require("sequelize");
-const utils = require("./utils.js");
-
+const utils = require("./utils");
+const { Utils } = require('sequelize');
+const sequelize = require('sequelize');
+const claveToken=' eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9 ';
 //const cors = require('cors');
 const api = express();
 
@@ -11,20 +13,7 @@ api.listen(3000, (req,res) => {
     console.log('Servidor Delilah resto activo...')
 });
 
-
-api.post('/login', (req, res) => {
-    //console.log(req);
-    const { usuario_o_mail, password } = req.body;
-    const usuarioEncontrado = comprobarCuentaIngreso(usuario_o_mail, password);
-    if (!usuarioEncontrado) {
-        res.send('Error, no se encontro usuario/mail inexistente en el sistema');
-        res.status(statusError);
-    }
-    res.send('Ok. Ingreso exitoso');
-
-    res.json(req.body);
-});
-api.post('/usuario', (req, res, next) => {
+api.post('/usuarios', (req, res, next) => {
     const { usuario, nombre_ape, mail, telefono, domicilio, psw } = req.query;
     const usuarioAComprobar = comprobarCuentaIngreso(usuario);
     const mailAComprobar = comprobarCuentaIngreso(mail);
@@ -38,8 +27,32 @@ api.post('/usuario', (req, res, next) => {
         res.status(statusError).json("Usuario/mail existente");
     }
 });
+api.get('/usuarios', (req, res) => {
+    //console.log(req);
+    const { usuario_o_mail, password } = req.body;
+    const usuarioEncontrado = comprobarCuentaIngreso(usuario_o_mail, password);
+    if (!usuarioEncontrado) {
+        res.send('Error, no se encontro usuario/mail inexistente en el sistema');
+        res.status(utils.estadoDeServer.statusErrorCliente);
+    }
+    res.send('Ok. Ingreso exitoso');
+
+    res.json(req.body);
+});
+
 api.get('/productos', (req, res) => {
     const listaProductos = listarTodosProductos();
+});
+api.post('/login',(req,res)=>{
+ let {usuario_o_mail,password}=req.body;
+ const usuarios= await traerTabla("usuarios");
+ let ingresoXusuario=usuarios.find(unUsuario=>unUsuario.usuario==usuario_o_mail);
+ let ingresoXmail=usuarios.find((unUsuario=>unUsuario.mail==usuario_o_mail))
+ if(ingresoXusuario.rol===true){
+    let token=jwt.sign({usuario,rol:'admin'},claveToken);
+    res.status(201).json({token});
+    console.log()
+ }
 });
 
 function comprobarCuentaIngreso(usuario_o_mail, password) {
@@ -56,7 +69,11 @@ function buscarEnDB(unaTabla, unaColumna) {
     //revisar la busqueda
   //  const searchResults = await sequelize.query(`SELECT * FROM ${unaTabla} WHERE ${unaColumna} = :replacementParam`);
 }
-
+async function traerTabla(unaTabla){
+const unaTabla= await sequelize.query('SELECT * FROM ${unatabla}',{
+    type:sequelize.QueryTypes.SELECT
+})
+}
 async function validate_token(req, res, next) {
     const token = req.headers.authorization.split(" ")[1];
     try {
@@ -76,6 +93,48 @@ async function validate_token(req, res, next) {
         res.status(401).json("Unauthorized - Invalid Token");
     }
 }
+//el usuario autenticado es aquel con el rol admin
+function autenticarUsuario(req,res,next){
+    try{
+        const token=req.headers.authorization.split(' ')[1];
+        const autenticarToken=jwt.verify(token,claveToken) ;
+        if(autenticarToken){
+            req.usuario=autenticarToken;
+            console.log(utils.mensajeServer.statusOkValidacionMensaje);
+            next();
+        }
+    }
+    catch(error){
+        console.log(utils.mensajeServer.statusErrorCredencialesMensaje);
+        res.status().json('Error al validar usuario');
+    }
+
+}
+
+//cuando el usuario haga una peticion sobre un pedido especifico verifico que ese pedido sea propio
+function autenticarConsultaDeUsuario(req, res,next){
+    try{
+        const token=req.headers.authorization.split(' ')[1];
+        const autenticarToken=jwt.verify(token,claveToken) ;
+        console.log(autenticarToken.token);
+        if(autenticarToken.usuario===req.params.usuario){
+            req.usuario=autenticarToken;
+            console.log('Usuario verificado');
+            next();
+        }
+        else{
+            //verificar si toma el valor del import
+            res.status(utils.statusErrorCredenciales).json('Error el usuario no esta autorizado');
+        }
+    }
+    catch(error)
+    {
+        console.log('Error usuario no autenticado');
+        res.status(utils.statusErrorCredenciales).json('Error al verificar usuario');
+    }
+    
+}
+
 
 function comprobarIngreso(usuario_o_mail, password) {
     //query para que busque los productos
--- utils.js
+++ utils.js
@@ -1,13 +1,23 @@
 const statusOk = 200,
+    statusOkValidacion=201,
     statusErrorCliente = 400,//bad request
     statusErrorCredenciales=401,//Unauthorized
     statusNotFound=404,
     statusErrorServidor=500;//Internal Server Error
-
+    const statusOkMensaje = 'Ingreso exitoso',
+    statusOkValidacionMensaje="Usuario verificado",
+    statusErrorClienteMensaje ='Peticion Erronea',//bad request
+    statusErrorCredencialesMensaje='Credencial inválida, no puede ingresar',//Unauthorized
+    statusNotFoundMensaje='status not found',
+    statusErrorServidorMensaje='Internal Server Error';//Internal Server Error
 //def estados de los pedidos
 const pedidoNuevo=10,pedidoConfirmado=20,pedidoPreparado=30,pedidoEnviado=40,pedidoCancelado=50,pedidoEntregado=60;
 
-
+const estadoPedidos={
+    pedidoNuevo,pedidoConfirmado,pedidoPreparado,pedidoCancelado,pedidoEntregado
+}
+const estadoDeServer={statusOk,statusOkValidacion,statusErrorCliente,statusErrorCredenciales,statusNotFound,statusErrorServidor}
+const mensajeServer={statusOkMensaje,statusOkValidacionMensaje,statusErrorClienteMensaje,statusErrorCredencialesMensaje,statusNotFoundMensaje,statusErrorServidorMensaje};
 //prueba para ejecutar la consola
 let usuarios = [{
     nombre_ape: "lolo pari",
@@ -26,3 +36,4 @@ let usuarios = [{
     rol: false
 }
 ];
+module.exports = { estadoDeServer,estadoPedidos,mensajeServer};
